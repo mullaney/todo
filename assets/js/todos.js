7f464f218ca298397af7fff71aa8a20905a0bbc2
Kevin Kelly <kvinklly@gmail.com>
2018-01-01 00:44:03 -0600

    functionality done

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,10 +1,23 @@
-$("li").click(function() {
+$("ul").on("click", "li", function() {
   $(this).toggleClass("completed");
-})
+});
 
-$("li > span").click(function(event) {
+$("ul").on("click", "span", function(event) {
   $(this).parent().fadeOut(800, function() {
     $(this).remove();
   });
   event.stopPropagation();
-})
+});
+
+$("input[type='text']").keypress(function(event) {
+  if (event.which === 13) {
+    if ($(this).val().trim() != '') {
+      $('ul').append('<li><span><i class="fa fa-trash-o" aria-hidden="true"></i></span>' + $(this).val() + '</li>');
+    }
+    $(this).val('');
+  }
+});
+
+$('.fa-pencil').click(function() {
+  $("input[type='text']").fadeToggle(200);
+});
